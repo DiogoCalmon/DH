1f81ee145fb472a4508abd47ffed480bb96924b3
pedroPivattoCavilha <ppcavilha@gmail.com>
2022-03-22 21:23:57 -0300

    update A

--- A.js
+++ A.js
@@ -1,10 +1,2 @@
 // nome: Diogo Calmon
-<<<<<<< HEAD
-<<<<<<< HEAD
-//cidade: Osasco
-=======
-//cidade: Salvador-BA......
->>>>>>> 1ca88ae285ea8dea8fcded86a51ab3b4e777f4e1
-=======
 //cidade: Joinville
->>>>>>> 08c0633ffc25e8ab0390413f43c8e7b99163efd8
